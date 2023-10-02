7bad1bf11b559f4cb3e049781adc063bb0f2b012
huise0ng <shinhuiseong07@gmail.com>
2023-10-02 15:46:32 +0900

    ADD :: jsconfig File 추가

--- main.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-// Formatting, Linting
-// Formatting: Prettier
-// Linting: ESLint
-
-const someString = 'Hello'
-const result = Math.log(someString)
-console.log(result)
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// @ts-check
+// Formatting, Linting
+// Formatting: Prettier
+// Linting: ESLint
+// Type checking: TypeScript
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.end('Hello!')
+})
+
+const PORT = 4000
+server.listen(PORT, () => {
+    console.log(`The server is listening at port: ${PORT}.`)
+})
