ba07df37941f32fa64f09900e76c55535604f015
huise0ng <shinhuiseong07@gmail.com>
2023-10-19 10:17:51 +0900

    ADD :: errorbook

---
+++ Errorbook.md
@@ -0,0 +1,14 @@
+# Node.js 에러 모음집
+### node: command not found : 환경설정에러
+### ReferenceError: not defined module (require)
+### Cannot find module : not download module (pip install ~~)
+### Can't set headers after they are sent : 응답을 2번 이상 보냈을 경우에 뜨는 에러
+### Call_AMD_RETRY_LAST ALLORCATION FAILED : Javascript 용량 부족
+### UnhandIedpromiseRejectionWarning: not catch method
+### EADDINUSE: Port number 중복
+### EACCES OR EPERM : 작업 수행 중 권한 부족
+### EJSONPARSE: package.json File 문법 error
+### ECONNREFUSED: 요청은 보냈으나 연결이 성립하지 않음
+### ETARGET: package.json version error
+### ETIMEOUT: 용청을 보냈으나 응답이 일정시간 이내에 오지 않음
+### ENOENT: 지정한 폴더나 파일이 존재하지 않을 경우
--- src/main.js
+++ src/main.js
@@ -8,31 +8,33 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
   const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
-  const postIdRegexResult = req.url && POSTS_ID_REGEX.exec(req.url) || undefined
-  
+  const postIdRegexResult =
+    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
+
   if (req.url === '/posts' && req.method === 'GET') {
     res.statusCode = 200
     res.end('List of posts')
-  } else if (postIdRegexResult) { 
+  } else if (postIdRegexResult) {
     //GET /post/:id
     const postId = postIdRegexResult[1]
     console.log(`postid: ${postId}`)
-    res.statusCode = 200
+    res.statusCode = 20041
+  
     res.end('Reading a post')
   } else if (req.url === '/posts' && req.method === 'POST') {
     res.statusCode = 200
     res.end('Creating post')
-  } else{
+  } else {
     res.statusCode = 404
     res.end('Not found.')
   }
 
-    res.statusCode = 200
-    res.end('Hello!')
+  res.statusCode = 200
+  res.end('Hello!')
 })
 
 const PORT = 4000
 
 server.listen(PORT, () => {
-    console.log(`The server is listening at port: ${PORT}.`)
+  console.log(`The server is listening at port: ${PORT}.`)
 })
